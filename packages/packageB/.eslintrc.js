5a3244ee2e64d9ac7cf7dc53c09b29ea772af4a3
takumiya081 <takumiya081@gmail.com>
2022-09-26 16:31:36 +0900

    update settings

--- packages/packageB/.eslintrc.js
+++ packages/packageB/.eslintrc.js
@@ -1,41 +1,21 @@
 const path = require('path');
 
-const ERROR = 'error';
-
 module.exports = {
   overrides: [
-    {
-      files: ['**/*.graphql'],
+     {
+      // Setup GraphQL Parser
+      files: '*.{graphql,gql}',
       parser: '@graphql-eslint/eslint-plugin',
       plugins: ['@graphql-eslint'],
       parserOptions: {
         schema: path.resolve(__dirname, '../packageA/schema.graphql'),
         operations: [path.resolve(__dirname, './src/**/*.graphql')],
+        skipGraphQLConfig: true
       },
-      rules: {
-        '@graphql-eslint/no-anonymous-operations': ERROR,
-        '@graphql-eslint/require-id-when-available': ERROR,
-        '@graphql-eslint/validate-against-schema': ERROR,
-        '@graphql-eslint/require-deprecation-reason': ERROR,
-        '@graphql-eslint/no-case-insensitive-enum-values-duplicates': ERROR,
-        '@graphql-eslint/require-description': [
-          ERROR,
-        // https://github.com/dotansimha/graphql-eslint/blob/master/docs/rules/require-description.md#configuration
-        {
-            on: [
-              'ObjectTypeDefinition',
-              'FieldDefinition',
-              'InputValueDefinition',
-              'InterfaceTypeDefinition',
-              'UnionTypeDefinition',
-              'EnumTypeDefinition',
-              'EnumValueDefinition',
-              'InputObjectTypeDefinition',
-              'DirectiveDefinition',
-          ],
-        },
-      ],
     },
-  },
-],
-}
+    {
+      files: ['**/*.graphql'],
+      extends: 'plugin:@graphql-eslint/operations-all',
+    },
+  ]
+};
